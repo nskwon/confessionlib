5bef53f43dda45ca7b15fdf103c31333124e2ba7
nskwon <nskwon@ucsd.edu>
2020-07-03 15:48:35 -0700

    Added next confession functionality

--- confessionlist/client/src/app/confessions/confessions.component.ts
+++ confessionlist/client/src/app/confessions/confessions.component.ts
@@ -1,19 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-import {ConfessionService} from '../confession.service';
-
+import { ConfessionService } from '../confession.service';
+import { Confession } from '../confession';
 @Component({
   selector: 'app-confessions',
   templateUrl: './confessions.component.html',
   styleUrls: ['./confessions.component.css'],
 })
 export class ConfessionsComponent implements OnInit {
-  newcomponent = "Entered in new component created"; 
-  todaydate; 
-  newcomponentproperty; 
-  newcomp = "Entered in newcomp";
-  constructor(private confessionservice: ConfessionService) { }
+
+  title = 'ConfessionLib';
+  constructor(private confessionservice: ConfessionService) {}
+  public confessiondata = [];
+  tempconfessiondata = [];
+  item : Confession;
+  index = 0;
+
+  nextConfession(){
+    if(this.confessiondata.length > 1){
+      this.confessiondata.splice(this.index,1);
+      console.log("Current length of array: " + this.confessiondata.length);
+      this.index = Math.floor(Math.random() * Math.floor(this.confessiondata.length))
+      this.item = this.confessiondata[this.index];
+    }
+    else{
+      console.log("In the else statement of nextConfession");
+      this.confessionservice.getConfessionList().subscribe((data) => {
+        this.confessiondata = Array.from(Object.keys(data), k=>data[k]);
+        this.index = Math.floor(Math.random() * Math.floor(this.confessiondata.length))
+        this.item = this.confessiondata[this.index];
+      })
+    }
+  }
+
   ngOnInit() { 
-    this.todaydate = this.confessionservice.showTodayDate(); 
-    this.newcomponentproperty = this.confessionservice.serviceproperty;
+    this.confessionservice.getConfessionList().subscribe((data) => {
+      this.confessiondata = Array.from(Object.keys(data), k=>data[k]);
+      console.log(this.confessiondata);
+      this.index = Math.floor(Math.random() * Math.floor(this.confessiondata.length))
+      this.item = this.confessiondata[this.index];
+    });
   } 
 }
