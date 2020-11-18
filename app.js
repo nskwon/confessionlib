3043ed1d5e693614362ce4bb7ca143b0edaf84a2
nskwon <nskwon@ucsd.edu>
2020-11-17 21:29:41 -0800

    Removing duplicate code

--- app.js
+++ app.js
@@ -34,19 +34,6 @@ client.connect(err => {
   var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
-  });if (err) {
-    console.log(err);
-    process.exit(1);
-  }
-
-  // Save database object from the callback for reuse.
-  db = client.db();
-  console.log("Database connection ready");
-
-  // Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
   });
 });
 
